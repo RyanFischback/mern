77609834c3da0eee1079d80917d53ca075307b8c
RyanFischback <ry762981@dal.ca>
2022-08-18 16:03:45 -0300

    user controller, frontend, redux, login function

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -6,7 +6,7 @@ const Goal = require('../models/goalModel')
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({user: req.user.id})
     res.status(200).json(goals)
 })
 
@@ -20,7 +20,8 @@ const postGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
@@ -35,6 +36,12 @@ const updateGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("Goal not Found")
     }
+    if (!req.user) 
+    {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body)
 
     res.status(200).json(updatedGoal)
@@ -49,6 +56,11 @@ const deleteGoal = asyncHandler(async (req, res) => {
     {
         res.status(400)
         throw new Error("Goal not Found")
+    }
+    if (!req.user) 
+    {
+        res.status(401)
+        throw new Error('User not found')
     }
     await goal.remove()
     res.status(200).json({id: req.params.id})
